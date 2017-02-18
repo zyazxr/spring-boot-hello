45f9f246c5c6be0ce65a9b2189578a1f6cc0363a
zhaoyuwy <zhaoyuwy@163.com>
2017-02-18 18:00:29 +0800

    HelloWord SpringBoot Fastjson createTime 第二种方法Bean

--- src/main/java/com/zy/App.java
+++ src/main/java/com/zy/App.java
@@ -6,6 +6,8 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,24 +18,37 @@ import java.util.List;
  * 使用SpringBootApplication 指定这是一个spring boot 程序
  */
 @SpringBootApplication
-public class App extends WebMvcConfigurerAdapter {
+//public class App extends WebMvcConfigurerAdapter {
+public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @Override
+/*    @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         super.configureMessageConverters(converters);
-        /**
-         * 1.需要先定义一个Convert 转换消息对象
-         * 2.添加FastJson 的配置信息，比如，是佛要格式化jsonshuju
-         * 3.在Convert中添加配置信息
-         * 4.将
-         */
+        */
+
+    /**
+     * 1.需要先定义一个Convert 转换消息对象
+     * 2.添加FastJson 的配置信息，比如，是佛要格式化jsonshuju
+     * 3.在Convert中添加配置信息
+     * 4.将
+     *//*
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
         fastConverter.setFastJsonConfig(fastJsonConfig);
         converters.add(fastConverter);
+    }*/
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
     }
 }
