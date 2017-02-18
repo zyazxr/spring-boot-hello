f7249478057864ddeb347d986fa92e53171d1f7f
zhaoyuwy <zhaoyuwy@163.com>
2017-02-18 17:53:29 +0800

    HelloWord SpringBoot Fastjson createTime

--- src/main/java/com/zy/Demo.java
+++ src/main/java/com/zy/Demo.java
@@ -1,11 +1,26 @@
 package com.zy;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/2/18.
  * 测试一个实体类
  */
 public class Demo {
     private int id;
+
+    @JSONField(format = "yyyy-mm-dd HH:MM:SS")
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    private Date createTime;
 
     public int getId() {
         return id;
--- src/main/java/com/zy/HelloController.java
+++ src/main/java/com/zy/HelloController.java
@@ -3,6 +3,8 @@ package com.zy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/2/18.
  * 使用RestController 等价于 @Controller和 @RequestBody
@@ -28,6 +30,7 @@ public class HelloController {
         Demo demo = new Demo();
         demo.setId(1);
         demo.setName("zhangsan");
+        demo.setCreateTime(new Date());
         return demo;
     }
 }
