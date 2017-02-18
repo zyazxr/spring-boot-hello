e2050f220e02396f533130facb45588ac651b391
zhaoyuwy <zhaoyuwy@163.com>
2017-02-18 17:50:29 +0800

    HelloWord SpringBoot Fastjson

--- src/main/java/com/zy/App.java
+++ src/main/java/com/zy/App.java
@@ -1,15 +1,39 @@
 package com.zy;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/2/18.
  * 使用SpringBootApplication 指定这是一个spring boot 程序
  */
 @SpringBootApplication
-public class App {
+public class App extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        /**
+         * 1.需要先定义一个Convert 转换消息对象
+         * 2.添加FastJson 的配置信息，比如，是佛要格式化jsonshuju
+         * 3.在Convert中添加配置信息
+         * 4.将
+         */
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
     }
 }
